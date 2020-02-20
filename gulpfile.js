68328358e2d66afa9c325bc7fa206a73bc338736
Mark Sharp <m.sharp@chi.ac.uk>
2020-02-20 12:06:14 +0000

    updated gulp to gulp4.x

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,31 @@
-// https://www.sitepoint.com/simple-gulpy-workflow-sass/
+"use strict";
+const { src, dest, watch, series } = require('gulp');
+const notify = require('gulp-notify');
+const cleanCss = require('gulp-clean-css');
+const run = require('gulp-run');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-var minifyCss = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
-var notify = require('gulp-notify');
 // var uglify = require('gulp-uglify');
 // var rename = require('gulp-rename');
-var run = require('gulp-run');
 
-gulp.task('sass', function() {
-    return gulp.src('./sass/styles.scss')
+function sassy() {
+    return src('./sass/styles.scss')
         .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
-            .pipe(minifyCss())
+            .pipe(cleanCss())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('.'))
+        .pipe(dest('.'))
         .pipe(notify("CSS Compiled!"));
-});
+}
 
-// gulp.task('jsmin', function() {
-//     return gulp.src(['./jquery/turnitintooltwo*.js', '!./jquery/turnitintooltwo*.min.js'])
-//     .pipe(sourcemaps.init())
-//     .pipe(uglify())
-//     .pipe(rename({suffix: '.min'}))
-//     .pipe(sourcemaps.write())
-//     .pipe(gulp.dest('./jquery/'))
-//     .pipe(notify('js minified'));
-// });
-
-gulp.task('decache', function() {
+function decache() {
     return run('/usr/bin/php ../../admin/cli/purge_caches.php').exec();
-});
+}
 
-gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', ['sass', 'decache']);
-    // gulp.watch('./jquery/turnitintooltwo*.js', ['jsmin']);
-});
+function watchme() {
+    watch('./sass/**/*.scss', series(sassy, decache));
+}
 
-gulp.task('default', ['sass', 'watch']);
+exports.default = watchme;
+exports.sass = sassy;
