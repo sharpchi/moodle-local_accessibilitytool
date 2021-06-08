1be4fef65ff99e9db722e435f8550de6d545d75e
Mark Sharp <m.sharp@chi.ac.uk>
2021-06-08 10:39:37 +0100

    Removed gulp components no longer being used

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,17 @@
 const { src, dest, watch, series } = require('gulp');
 const notify = require('gulp-notify');
 const cleanCss = require('gulp-clean-css');
-// const run = require('gulp-run');
-// const shell = require('gulp-shell');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-
-// var uglify = require('gulp-uglify');
-// var rename = require('gulp-rename');
 
 function sassy() {
     return src('./sass/styles.scss')
         .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-            .pipe(cleanCss())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCss())
         .pipe(sourcemaps.write('.'))
         .pipe(dest('.'))
         .pipe(notify("CSS Compiled!"));
-}
-
-/**
- * Removed run because it's no longer safe to use. Can't get shell to work.
- * Purge caches manually.
- */
-function decache() {
-    // return run('/usr/bin/php ../../admin/cli/purge_caches.php').exec();
-    shell.task('/usr/bin/php ../../admin/cli/purge_caches.php');
 }
 
 function watchme() {
