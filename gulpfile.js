8f7d62c9d701d079568f49c3bb35605fa646fb53
Mark Sharp <mark.sharp@solent.ac.uk>
2024-11-12 15:11:01 +0000

    Do not lint styles.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 "use strict";
-const { src, dest, watch, series } = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const notify = require('gulp-notify');
 const cleanCss = require('gulp-clean-css');
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const gap = require('gulp-append-prepend');
 
 function sassy() {
     return src('./scss/styles.scss')
@@ -15,9 +16,14 @@ function sassy() {
         .pipe(notify("CSS Compiled!"));
 }
 
+function bitolint() {
+    return src('./styles.css')
+        .pipe(gap.prependText('/* stylelint-disable */'))
+        .pipe(dest('.'));
+}
+
 function watchme() {
-    watch('./scss/**/*.scss', series(sassy));
+    watch('./scss/**/*.scss', series(sassy, bitolint));
 }
 
 exports.default = watchme;
-exports.sass = sassy;
